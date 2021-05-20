cbf14660cb290eed574e21ef6bcdc24208c11bf4
Philippe Tarbouriech <philippe@phitar.com>
2021-05-20 14:56:45 +0200

    UI stuff

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from easygraphics import *
 from easygraphics.dialog import *
 import csv
-import colorsys
 import os
 import requests
 import country_codes
+from PyQt5.QtGui import QFont
 
 # Country codes are ISO 3166-1
 # All referred to in slim-country.csv
@@ -14,40 +14,50 @@ import country_codes
 _Start_year = 1950
 _End_year = 2100
 
-_Vertical = 600
-_Horizontal = 1200
+_Vertical = 800
+_Horizontal = 1400
 _Margin = 50
+_ActiveColumns = 21
 _Columns = 21
 _Column_width = (_Horizontal - (2* _Margin)) / (_Columns + 2)
 
-_pink_hue = 320/360
+_pink_hue = 320
 _pink_saturation = 35/100
-_blue_hue= 208/360
+_blue_hue= 208
 _blue_saturation = 57/100
-_sex_scale = 0.1  # More than 10% delta means max saturation
+_sex_scale = 0.08  # More than 10% delta means max saturation
+
+_orange_color = "#FF6600"
+_blue_color = "#3399FF"
+_purple_color = "#663399"
+_green_color = "#55D43F"
+_SubFrames = 5
 
 
+def set_bar_color(m, f, dark, mode, color):
+    if mode == "sex":
+        if m+f == 0:
+            set_fill_color(color_rgb(255, 0, 0))
+            return
+        relative = m / (m + f) - 0.5
+        if relative > _sex_scale:
+            relative = _sex_scale
+        if relative < -_sex_scale:
+            relative = -_sex_scale
 
-def set_bar_color(m, f, dark):
-    if m+f == 0:
-        set_fill_color(color_rgb(255, 0, 0))
-        return
-    relative = m / (m + f) - 0.5
-    if relative > _sex_scale:
-        relative = _sex_scale
-    if relative < -_sex_scale:
-        relative = -_sex_scale
+        if relative > 0:
+            set_fill_color(color_hsv(_blue_hue, int(255*_blue_saturation*relative/_sex_scale), int(255*dark)))
+        else:
+            set_fill_color(color_hsv(_pink_hue, int(255*-_pink_saturation*relative/_sex_scale), int(255*dark)))
 
-    if relative > 0:
-        (red, green, blue) = colorsys.hsv_to_rgb(_blue_hue, _blue_saturation*relative/_sex_scale, dark)
     else:
-        (red, green, blue) = colorsys.hsv_to_rgb(_pink_hue, -_pink_saturation*relative/_sex_scale, dark)
-
-    cr, cg, cb = int(255 * red), int(255 * green), int(255 * blue)
-    set_fill_color(color_rgb(cr, cg, cb))
+        if len(color) != 7:
+            set_fill_color(color_rgb(255, 0, 0))
+        else:
+            set_fill_color(color)
 
 def mainloop(country, scale, max):
-    _Speed = 5  # frame per sec
+    Speed = 5  # frame per sec
 
     year = _Start_year
     set_color(color_rgb(80, 80, 80, 255))
@@ -55,22 +65,26 @@ def mainloop(country, scale, max):
     last_pop_f = []
     first_year = True
     initial_pop = []
+    ColorMode = "sex"
     # Initialize last population
-    for i in range (0,_Columns):
+    for i in range (0,_ActiveColumns):
         last_pop_m.append(0)
         last_pop_f.append(0)
         initial_pop.append(0)
     while is_run():
         if has_kb_hit():
             mykey = get_char()
-            if mykey == " ":
+            if mykey == "y":
                 pause()
             if mykey == "<":
-                if _Speed > 2:
-                    _Speed = _Speed - 1
+                if Speed > 2:
+                    Speed = Speed - 1
             if mykey == ">":
-                    _Speed = _Speed + 1
-
+                Speed = Speed + 1
+            if mykey == "s":
+                ColorMode = "sex"
+            if mykey == "c":
+                ColorMode = "color"
 
         if has_mouse_msg():
             while has_mouse_msg():
@@ -87,19 +101,19 @@ def mainloop(country, scale, max):
                 pop_m.append(int(row[1]))
                 pop_f.append(int(row[2]))
 
-        if delay_jfps(_Speed):
+        if delay_jfps(Speed):
             clear_device()
             startx = _Margin
-            starty = _Vertical - _Margin
+            starty = _Vertical / 2
             death_m = 0
             death_f = 0
             total_pop_m = 0
             total_pop_f = 0
-            for group in range(0,_Columns):
+            for group in range(0,_ActiveColumns):
                 if group == 0:
                     initial_pop[0] = pop_m[0] + pop_f[0]
-                draw_text(startx, starty + _Margin / 2, str(group*5))
-                set_bar_color(pop_m[group],pop_f[group],0.7)
+                draw_text(startx + 3, starty + _Margin / 2, str(group*5))
+                set_bar_color(pop_m[group],pop_f[group],0.7, ColorMode, _green_color)
                 total_pop_f = total_pop_f + pop_f[group]
                 total_pop_m = total_pop_m + pop_m[group]
                 if first_year or group == 0:
@@ -113,12 +127,12 @@ def mainloop(country, scale, max):
                     # DEATH
                     if delta_m+delta_f < 0:
                         draw_rect(startx, starty - scale * (pop_m[group]+pop_f[group]), startx + _Column_width, starty)
-                        set_bar_color(delta_m, delta_f, 0.4)
+                        set_bar_color(delta_m, delta_f, 0.4, ColorMode, _purple_color)
                         draw_rect(startx, starty - scale * (pop_m[group] + pop_f[group] - delta_m - delta_f) , startx + _Column_width, starty - scale * (pop_m[group]+pop_f[group]))
                     # IMMIGRATION
                     else:
                         draw_rect(startx, starty - scale * (pop_m[group] + pop_f[group] - delta_m - delta_f), startx + _Column_width, starty)
-                        set_bar_color(-delta_m, -delta_f, 0.8)
+                        set_bar_color(-delta_m, -delta_f, 0.8, ColorMode, _blue_color)
                         draw_rect(startx, starty - scale * (pop_m[group]+pop_f[group]), startx + _Column_width, starty - scale * (pop_m[group] + pop_f[group] - delta_m - delta_f))
 
                 if initial_pop[group] > 0:
@@ -128,24 +142,24 @@ def mainloop(country, scale, max):
                     set_color(color_rgb(80, 80, 80, 255))
 
                 if (year - group * 5) % 20 == 0:
-                    draw_text(startx, _Vertical - 3 * _Margin / 2, (year - 5 * group))
+                    draw_text(startx + 3, _Vertical / 2 - _Margin / 2, (year - 5 * group))
                 startx = startx + _Column_width
 
             startx = startx + _Column_width # Skip one column before death
             draw_text(startx, starty + _Margin / 2, "death")
-            set_bar_color(death_m, death_f, 0.4)
+            set_bar_color(death_m, death_f, 0.4, ColorMode, _purple_color)
             draw_rect(startx, starty + scale * (death_m + death_f), startx + _Column_width, starty)
 
             startx = _Margin
-            starty = _Vertical - _Margin
+            starty = _Vertical / 2
             draw_text(_Margin, _Margin/2, country," : ",year, " population ",int((total_pop_m+total_pop_f)/10000)/100, "M % of max : ", int(1000*(total_pop_m+total_pop_f)/max)/10)
 
         if first_year:
             first_year = False
-        for i in range(0, _Columns):
+        for i in range(0, _ActiveColumns):
             last_pop_m[i] = pop_m[i]
             last_pop_f[i] = pop_f[i]
-        for i in range(_Columns-1, 0, -1):
+        for i in range(_ActiveColumns-1, 0, -1):
             initial_pop[i] = initial_pop[i-1]
 
         # Move onto next year
@@ -159,10 +173,13 @@ def mainloop(country, scale, max):
             last_pop_f = []
             initial_pop = []
             # Initialize last population
-            for i in range (0,_Columns):
+            for i in range (0,_ActiveColumns):
                 last_pop_m.append(0)
                 last_pop_f.append(0)
                 initial_pop.append(0)
+
+    print ("EXIT")
+
 
 def find_max(country):
     year = _Start_year
@@ -218,6 +235,9 @@ def main():
 
     init_graph(_Horizontal, _Vertical)
     set_render_mode(RenderMode.RENDER_MANUAL)
+    set_font(QFont("Courier"))
+    set_font_size(14)
+    print(f"{get_font().family()} : {get_font_size()}")
     while True:
         reply = get_choice("What country", choices=country_codes.countries.keys())
         if reply == None:
@@ -235,13 +255,10 @@ def main():
                 while True:
                     if delay_jfps(1000):
                         clear_device()
-                        startx = _Margin
-                        starty = _Vertical - _Margin
-
                         draw_text(_Margin, _Margin / 2, reply, " loading cache : ", str(year))
-                        set_bar_color(1,1,0.4)
+                        set_bar_color(1,1,0.4, "color", "#A0A0A0")
                         draw_rect(_Margin, _Margin, _Margin + 4* (2100 - 1950), _Margin + 20)
-                        set_bar_color(2+(2100-year)/150,2+(year-1950)/150,0.8)
+                        set_bar_color(2+(2100-year)/150,2+(year-1950)/150,0.8,"color", "#3399ff")
                         draw_rect(_Margin, _Margin, _Margin + 4* (year - 1950), _Margin + 20)
                         url = 'https://www.populationpyramid.net/api/pp/' + str(country_codes.countries[reply]) + '/' + str(year)+ '/?csv=true'  # [1950-2100:5]
                         r = requests.get(url)
@@ -252,8 +269,6 @@ def main():
                         break
 
         (maxbar, maxtotal) = find_max(reply)
-        mainloop(reply, (_Vertical - 2 * _Margin) / maxbar, maxtotal)
+        mainloop(reply, (_Vertical / 2 - 2 * _Margin) / maxbar, maxtotal)
     close_graph()
-
-
 easy_run(main)
